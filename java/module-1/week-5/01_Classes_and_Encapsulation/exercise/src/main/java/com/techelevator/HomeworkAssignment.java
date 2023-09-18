34d6b7cfb740202e650a5fadb5a533c9b812c3ce
Dipina Kc <dipinakc89@gmail.com>
2023-09-18 12:36:23 -0400

    Encapsulation homeowrk

--- java/module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
+++ java/module-1/week-5/01_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/HomeworkAssignment.java
@@ -1,6 +1,42 @@
 package com.techelevator;
 
 public class HomeworkAssignment {
+    private  int earnedMarks;
+    private int possibleMarks;
+    private String submitterName;
 
+    public HomeworkAssignment(int possibleMarks, String submitterName){
+        this.possibleMarks = possibleMarks;
+        this.submitterName = submitterName;
 
+    }
+    public int getEarnedMarks(){
+        return earnedMarks;
+    }
+public void setEarnedMarks(int earnedMarks){
+        this.earnedMarks= earnedMarks;
+}
+
+    public int getPossibleMarks() {
+        return possibleMarks;
+    }
+
+    public String getSubmitterName() {
+        return submitterName;
+    }
+    public String getLetterGrade(){
+        double percentage = (double) earnedMarks / possibleMarks * 100;
+        if (percentage >= 90) {
+            return "A";
+
+        } else if (percentage >=80) {
+            return "B";
+        }else if (percentage>=70) {
+            return "C";
+        } else if (percentage >=60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
 }
