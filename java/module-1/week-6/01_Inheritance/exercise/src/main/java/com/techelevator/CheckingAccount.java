71769851f8aa6e0e9539249c2fda1be60a2b7377
Dipina Kc <dipinakc89@gmail.com>
2023-09-23 18:54:15 -0400

    Inheritance

--- java/module-1/week-6/01_Inheritance/exercise/src/main/java/com/techelevator/CheckingAccount.java
+++ java/module-1/week-6/01_Inheritance/exercise/src/main/java/com/techelevator/CheckingAccount.java
@@ -1,23 +1,34 @@
 package com.techelevator;
-
 public class CheckingAccount extends BankAccount {
     public CheckingAccount(String accountHolderName, String accountNumber) {
-        super(accountHolderName,accountNumber);
-
+        super(accountHolderName, accountNumber);
     }
+
     public CheckingAccount(String accountHolderName, String accountNumber, int balance) {
-       super(accountHolderName, accountNumber, balance);
+        super(accountHolderName, accountNumber, balance);
     }
 
     @Override
     public int withdraw(int amountToWithdraw) {
-        if (getBalance() >= amountToWithdraw) {
-            if (getBalance() < 0 && getBalance() >= -100) {
-            super.withdraw(amountToWithdraw + 10);
-            } else {
-              super.withdraw(amountToWithdraw);
+        if (getBalance() - amountToWithdraw >= -100 && amountToWithdraw >= 0) {
+            if (getBalance() - amountToWithdraw < 0) {
+                super.withdraw(10); // Apply overdraft fee
             }
+            return super.withdraw(amountToWithdraw);
         }
         return getBalance();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
