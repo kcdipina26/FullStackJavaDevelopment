146c5c998a9a5016187cee1c79dfa00a81548644
Dipina Kc <dipinakc89@gmail.com>
2023-10-07 18:41:55 -0400

    Week 8 reading

--- java/module-1/week-8/01_File_IO_Reading/exercise/src/main/java/com/techelevator/QuizMaker.java
+++ java/module-1/week-8/01_File_IO_Reading/exercise/src/main/java/com/techelevator/QuizMaker.java
@@ -1,5 +1,8 @@
 package com.techelevator;
 
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class QuizMaker {
@@ -15,7 +18,55 @@ public class QuizMaker {
 	public void run() {
 		/* Your code goes here */
 
+				System.out.println("Enter the fully qualified name of the file to read in for quiz questions:");
+				String filePath = userInput.nextLine();
 
-	}
+				int correctAnswers = 0;
+				int totalQuestions = 0;
 
-}
+				try {
+					Scanner fileScanner = new Scanner(new File(filePath));
+
+					while (fileScanner.hasNextLine()) {
+						String line = fileScanner.nextLine();
+						String[] parts = line.split("\\|");
+
+						System.out.println(parts[0]); // Print the question
+
+						// Identify correct answer
+						int correctIndex = -1;
+						for (int i = 1; i < parts.length; i++) {
+							if (parts[i].endsWith("*")) {
+								correctIndex = i - 1;
+								System.out.println(i + ". " + parts[i].substring(0, parts[i].length() - 1));
+							} else {
+								System.out.println(i + ". " + parts[i]);
+							}
+						}
+
+						System.out.print("Your answer: ");
+						int userAnswer = userInput.nextInt() - 1; // -1 to make it 0-based index
+						userInput.nextLine(); // Clear the new line
+
+						if (userAnswer == correctIndex) {
+							System.out.println("RIGHT!\n");
+							correctAnswers++;
+						} else {
+							System.out.println("WRONG!\n");
+						}
+
+						totalQuestions++;
+					}
+					fileScanner.close();
+				} catch (FileNotFoundException e) {
+					System.out.println("The specified file was not found.");
+				}
+
+				System.out.println("You got " + correctAnswers + " answer(s) correct out of " + totalQuestions + ".");
+			}
+		}
+
+
+
+
+
--- java/module-1/week-8/01_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ java/module-1/week-8/01_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,5 +1,7 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class WordSearch {
@@ -15,7 +17,57 @@ public class WordSearch {
 	public void run() {
 		/* Your code goes here */
 
+				// Prompt the user for the file path
+				System.out.println("Enter the fully qualified name of the file to be searched:");
+				String filePath = userInput.nextLine();
 
-	}
+				// Prompt the user for the search word
+				System.out.println("What is the search word you are looking for?");
+				String searchWord = userInput.nextLine();
 
-}
+				// Prompt the user for case sensitivity
+				System.out.println("Should the search be case sensitive? (Y/N)");
+				String caseSensitiveInput = userInput.nextLine();
+				boolean caseSensitive = caseSensitiveInput.equalsIgnoreCase("Y");
+
+				// Defined the separator character from instructions
+				String separator = ")"; // Use ")" as the separator
+
+				try {
+					Scanner fileScanner = new Scanner(new File(filePath));
+					int lineNumber = 1;
+
+					while (fileScanner.hasNextLine()) {
+						String line = fileScanner.nextLine();
+						String lineToSearch = caseSensitive ? line : line.toLowerCase();
+						String searchWordToLowerCase = caseSensitive ? searchWord : searchWord.toLowerCase();
+
+						if (lineToSearch.contains(searchWordToLowerCase)) {
+							// Output the matching line number and text with the specified separator
+							System.out.println(lineNumber + separator + " " + line);
+						}
+
+						lineNumber++;
+					}
+
+					fileScanner.close();
+				} catch (FileNotFoundException e) {
+					System.out.println("The specified file was not found.");
+				}
+			}
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
