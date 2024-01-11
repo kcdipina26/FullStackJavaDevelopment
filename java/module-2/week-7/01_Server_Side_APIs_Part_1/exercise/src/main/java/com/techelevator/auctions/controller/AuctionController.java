af195a405dfd842cf1e05b4659b152dee4649280
dipina kc <dipinakc89@gmail.com>
2024-01-11 18:54:45 -0500

    Week 7

--- java/module-2/week-7/01_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ java/module-2/week-7/01_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,7 +2,11 @@ package com.techelevator.auctions.controller;
 
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.dao.MemoryAuctionDao;
+import com.techelevator.auctions.model.Auction;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/auctions")
@@ -13,5 +17,28 @@ public class AuctionController {
     public AuctionController() {
         this.auctionDao = new MemoryAuctionDao();
     }
-
+@GetMapping
+public List<Auction> list(
+        @RequestParam(name = "title_like", required = false, defaultValue = "") String titleLike,
+        @RequestParam(name = "currentBid_lte", required = false, defaultValue = "0") double currentBidLte
+) {
+    if (!titleLike.isEmpty() && currentBidLte > 0) {
+        return auctionDao.getAuctionsByTitleAndMaxBid(titleLike, currentBidLte);
+    } else if (!titleLike.isEmpty()) {
+        return auctionDao.getAuctionsByTitle(titleLike);
+    } else if (currentBidLte > 0) {
+        return auctionDao.getAuctionsByMaxBid(currentBidLte);
+    } else {
+        return auctionDao.getAuctions();
+    }
+}
+@GetMapping("/{id}")
+    public Auction get(@PathVariable int id) {
+        return auctionDao.getAuctionById(id);
+}
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Auction create(@RequestBody Auction auction) {
+        return auctionDao.createAuction(auction);
+    }
 }
