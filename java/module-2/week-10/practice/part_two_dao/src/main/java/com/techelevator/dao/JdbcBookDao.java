488ba99277a837b2c78d03a58106c3b39c9e1df9
dipina kc <dipinakc89@gmail.com>
2024-01-19 17:20:46 -0500

    practice exam part 2

--- java/module-2/week-10/practice/part_two_dao/src/main/java/com/techelevator/dao/JdbcBookDao.java
+++ java/module-2/week-10/practice/part_two_dao/src/main/java/com/techelevator/dao/JdbcBookDao.java
@@ -1,6 +1,8 @@
 package com.techelevator.dao;
 
+import com.techelevator.exception.DaoException;
 import com.techelevator.model.Book;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 
@@ -13,23 +15,58 @@ public class JdbcBookDao implements BookDao {
 
     private final JdbcTemplate jdbcTemplate;
 
+
     public JdbcBookDao(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     @Override
-    public List<Book> getBooks() {
-        return new ArrayList<>();
+    public List<Book> getBooks() throws DaoException {
+            List<Book> books = new ArrayList<>();
+            String sql = "SELECT book_id, book_title, star_rating, out_of_print, foreword_by, publisher_id, published_date FROM book";
+            try {
+                SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+                while(results.next()) {
+                    books.add(mapRowToBook(results));
+                }
+            } catch (DataAccessException e) {
+                throw new DaoException("Error getting all books", e);
+            }
+            return books;
     }
 
     @Override
-    public Book getBookById(int bookId) {
-        throw new UnsupportedOperationException("Not implemented yet");
+    public Book getBookById(int bookId) throws DaoException {
+        String sql = "SELECT book_id, book_title, star_rating, out_of_print, foreword_by, publisher_id, published_date FROM book WHERE book_id = ?";
+        Book book = null;
+        try {
+            SqlRowSet results = jdbcTemplate.queryForRowSet(sql, bookId);
+            if (results.next()) {
+                book = mapRowToBook(results);
+
+            }
+        } catch (DataAccessException e) {
+            throw new DaoException("Error getting book with ID " + bookId, e);
+        }
+
+        return book;
     }
 
     @Override
     public Book updateBookRating(int bookId, BigDecimal newRating) {
-        throw new UnsupportedOperationException("Not implemented yet");
+
+        // First, check if the book exists
+        Book existingBook = getBookById(bookId);
+        if (existingBook == null) {
+            throw new DaoException("No book found with id " + bookId);
+        }
+
+        // Perform the update
+        String updateSql = "UPDATE book SET star_rating = ? WHERE book_id = ?";
+        jdbcTemplate.update(updateSql, newRating, bookId);
+
+        // Retrieve and return the updated book
+        return getBookById(bookId);
     }
 
     private Book mapRowToBook(SqlRowSet results) {
