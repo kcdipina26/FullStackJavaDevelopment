55fae58af74d56b91edefdaae226a222109f9ba6
dipina kc <dipinakc89@gmail.com>
2024-02-12 23:49:53 -0500

    Module 3 assignment

--- java/module-2/week-10/assessment/part_one_sql/queries/05_authors_and_forewords.sql
+++ java/module-2/week-10/assessment/part_one_sql/queries/05_authors_and_forewords.sql
@@ -2,4 +2,7 @@
 -- Order by book title (A-Z).
 -- Tip: make sure to add a space between the author's first and last names.
 -- (5 rows)
-
+SELECT book.book_title, person.first_name, ||''|| person.last_name AS foreword_Author
+FROM book
+JOIN person ON book.foreword_by = person.person_id
+SELECT book
