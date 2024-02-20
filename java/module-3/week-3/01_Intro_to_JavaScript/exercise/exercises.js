9b8e625825146258b5eead3d599e8932e860869a
dipina kc <dipinakc89@gmail.com>
2024-02-20 01:22:37 -0500

    Intro to javascript

--- java/module-3/week-3/01_Intro_to_JavaScript/exercise/exercises.js
+++ java/module-3/week-3/01_Intro_to_JavaScript/exercise/exercises.js
@@ -5,13 +5,16 @@
 		sumDouble(1, 2) → 3
 		sumDouble(3, 2) → 5
 		sumDouble(2, 2) → 8
-
-		function sumDouble(x, y) {
-			// do logic here
-			// return result;
-			return x + y;
-        }
 */
+		function sumDouble(x, y) {
+			if(x==y) {
+				return 2 * (x + y);
+			} else {
+             return x+y;
+			}
+			
+        }
+
 
 /*
 2. **hasTeen** We'll say that a number is "teen" if it is in the range 13..19 inclusive. 
@@ -21,6 +24,12 @@
 		hasTeen(20, 19, 10) → true
 		hasTeen(20, 10, 13) → true
 */
+      function hasTeen(a,b,c) {
+		var isATeen = a >= 13 && a <= 19;
+		var isBTeen = b >= 13 && b <= 19;
+		var isCTeen = c >= 13 && c <= 19;
+		return isATeen || isBTeen || isCTeen;
+	  }
 
 /* 
 3. **lastDigit** Given two non-negative int values, return true if they have the same 
@@ -30,6 +39,9 @@
 		lastDigit(6, 17) → false
 		lastDigit(3, 113) → true
 */
+      function lastDigit(a, b) {
+		return (a % 10) === (b % 10);
+	  }
 
 /*
 4. **seeColor** Given a string, if the string begins with "red" or "blue" return that color 
@@ -39,6 +51,15 @@
 		seeColor("xxred") → ""
         seeColor("blueTimes") → "blue"
 */
+      function seeColor(str) {
+		if (str.startsWith("red")) {
+			return "red";
+		} else if (str.startsWith("blue")) {
+			return "blue";
+		} else {
+			return "";
+		}
+		}
 
 /*
 5. **oddOnly** Write a function that given an array of integer of any length, removes
@@ -47,6 +68,9 @@
 		oddOnly([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]) → [1, 3, 5, 7, 9, 11];
 		oddOnly([2, 4, 8, 32, 256]); → []
 */
+       function oddOnly(arr) {
+		return arr.filter(num => num % 2 !== 0);
+	   }
 
 /*
 6. **frontAgain** Given a string, return true if the first 2 chars in the string also appear 
@@ -56,6 +80,9 @@
 		frontAgain("edit") → false
 		frontAgain("ed") → true
 */
+   function frontAgain(str) {
+	return str.substring(0, 2) == str.substring(str.length - 2);
+   }
 
 /*
 7. **cigarParty** When squirrels get together for a party, they like to have cigars. 
@@ -68,6 +95,14 @@ or false otherwise.
 		cigarParty(50, false) → true
 		cigarParty(70, true) → true
 */
+   function cigarParty(cigars, isWeekend) {
+	if (isWeekend){
+		return cigars >= 40;
+	} else{
+		return cigars >= 40 && cigars <= 60;
+	}
+	}
+       
 
 /*
 8. **fizzBuzz** Given a number, return a value according to the following rules:
@@ -82,6 +117,18 @@ In all other cases return the original number.
 	fizzBuzz(15) → "FizzBuzz"
 	fizzBuzz(8) → 8
 */
+function fizzBuzz(num) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        return "FizzBuzz";
+    } else if (num % 3 === 0) {
+        return "Fizz";
+    } else if (num % 5 === 0) {
+        return "Buzz";
+    } else {
+        return num;
+    }
+}
+
 
 /*
 9. **filterEvens** Write a function that filters an array to only include even numbers.
@@ -91,6 +138,9 @@ In all other cases return the original number.
 	filterEvens([2, 4, 6]) → [2, 4, 6]
 	filterEvens([100, 8, 21, 24, 62, 9, 7]) → [100, 8, 24, 62]
 */
+     function filterEvens(arr) {
+    return arr.filter(num => num % 2 === 0);
+}
 
 /*
 10. **filterBigNumbers** Write a function that filters numbers greater than or equal to 100.
@@ -99,6 +149,9 @@ In all other cases return the original number.
 	filterBigNumbers([3, 2, 7, 1, -100, -120]) → []
 	filterBigNumbers([]) → []
 */
+       function filterBigNumbers(arr) {
+       return arr.filter(num => num >= 100);
+}
 
 /*
 11. **filterMultiplesOfX** Write a function to filter numbers that are a multiple of a 
@@ -107,6 +160,10 @@ parameter, `x` passed in.
 	filterMultiplesOfX([3, 5, 1, 9, 18, 21, 42, 67], 3) → [3, 9, 18, 21, 42]
 	filterMultiplesOfX([3, 5, 10, 20, 18, 21, 42, 67], 5) → [5, 10, 20]
 */
+       function filterMultiplesOfX(arr, x) {
+           return arr.filter(num => num % x === 0);
+}
+
 
 /*
 12. **createObject** Write a function that creates an object with a property called 
@@ -120,3 +177,10 @@ firstName, lastName, and age. Populate the properties with your values.
 		age
 	}
 */
+    function createObject() {
+		return {
+			firstName: "Dipina",
+			lastName: "KC",
+			age: 26
+		};
+	}
