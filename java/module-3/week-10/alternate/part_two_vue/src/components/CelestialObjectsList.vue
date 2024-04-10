894ba0a71afabf47a00dee3da2e36e70494ea0a6
Myron Law <myron@techelevator.com>
2024-04-10 17:44:48 -0400

    Initial push of Mod3 alternate assessment

---
+++ java/module-3/week-10/alternate/part_two_vue/src/components/CelestialObjectsList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="card-container">
+    {{JSON.stringify(celestialObjectsList)}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['celestialObjectsList']
+}
+</script>
+
+<style scoped>
+#card-container {
+  color: white;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  justify-content: space-around;
+}
+</style>
